62ceb455582ebf60848d9304802434b04902e1c3
Hartikainen Ari <ari.hartikainen@aalto.fi>
2018-06-28 12:52:03 +0300

    hello_gurl init

--- pystan/__init__.py
+++ pystan/__init__.py
@@ -11,11 +11,13 @@ from pystan.misc import read_rdump, stan_rdump, stansummary
 from pystan.diagnostics import check_hmc_diagnostics
 from pystan.model import StanModel
 from pystan.lookup import lookup
+from emoji import emojize
 
 logger = logging.getLogger('pystan')
 logger.addHandler(logging.NullHandler())
 if len(logger.handlers) == 1:
-    logging.basicConfig(level=logging.INFO)
+    logging_format = "Hey Gurl {}: ".format(emojize(":rainbow:"))
+    logging.basicConfig(level=logging.INFO, format=logging_format)
 
 # following PEP 386
-__version__ = '2.17.1.0'
+__version__ = '2.17.1.0-HeyGurl'
